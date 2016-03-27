d05412a72631da913917f65302eef15332d5b846
Marcin Kossowski <marcin.kossowski@24hr.se>
2016-03-27 21:53:23 +0200

    Added Nyancat. Now I need to remove the cube

--- notes.js
+++ notes.js
@@ -2,3 +2,5 @@
 // Yeoman is basically used to create a boilerplate project.
 // "python -m http.server" will start a local server from anywhere in windows btw!
 //		To access the server, provide localhost with specified port, for instance http://localhost:8001/ if you type "python -m http.server 8001"
+// What is threeX?
+//		It is basically an ultra light extension system for three.js. 
