8684fa5f9fbd4cef23b33058e4e10d8bbdff5b30
Marcin Kossowski <marcin.kossowski@24hr.se>
2016-03-27 21:15:30 +0200

    Basic setup and working three js example

---
+++ notes.js
@@ -0,0 +1,4 @@
+/**27-03-2016 09:01 MK 
+// Yeoman is basically used to create a boilerplate project.
+// "python -m http.server" will start a local server from anywhere in windows btw!
+//		To access the server, provide localhost with specified port, for instance http://localhost:8001/ if you type "python -m http.server 8001"
